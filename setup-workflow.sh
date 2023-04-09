80c1240db5c3de3dbf9cd4d9303c483eec0d2f78
drbngd <55194087+drbngd@users.noreply.github.com>
2023-04-08 21:06:06 -0500

    added code to install htk

--- setup-workflow.sh
+++ setup-workflow.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+# unzip htk
+sudo tar -xvf htk_files/HTK-3.4.1.tar.gz
+sudo bpm-update htk_local
+
+# update pip version
+sudo python -m pip install -U pip
+
 # these are the dependencies to be installed for this project
- pip install pandas
- pip install openpyxl
- pip install numpy
- pip install XlsxWriter
+pip install psutil
+pip install pandas
+pip install openpyxl
+pip install numpy
+pip install XlsxWriter
  
