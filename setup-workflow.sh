60f34c7d62a54fcd63e3d0c3fc07038289172721
drbngd <55194087+drbngd@users.noreply.github.com>
2023-04-09 12:22:02 -0500

    Update setup-workflow.sh

--- setup-workflow.sh
+++ setup-workflow.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # unzip htk
-sudo tar -xvf htk_files/HTK-3.4.1.tar.gz
+cd htk_files/
 sudo bpm-update htk_local
+cd ..
 
 # update pip version
-sudo python -m pip install -U pip
+sudo apt-get update
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
 
 # these are the dependencies to be installed for this project
 pip install psutil
@@ -13,4 +16,3 @@ pip install pandas
 pip install openpyxl
 pip install numpy
 pip install XlsxWriter
- 
